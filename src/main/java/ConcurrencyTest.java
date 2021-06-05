17d399514c6f806694729b03721387631abac58c
victoralcant <v.alcantara@celonis.com>
2021-06-06 01:02:15 +0200

    initial

---
+++ src/main/java/ConcurrencyTest.java
@@ -0,0 +1,9 @@
+public class ConcurrencyTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Thread main");
+        Thread.sleep(3000);
+
+    }
+}
